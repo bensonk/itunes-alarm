5dd4ee65b3495caf5298a4fcba5192d96924b357
bensonk <d3x647@WE16382.pnl.gov>
2010-04-01 13:53:32 -0700

    Now supports times that occur tomorrow.
    If you enter a time that has already happened in the current day, the
    script now assumes you mean a time tomorrow.  This way, the example will
    work even if you set the alarm before midnight.  This seemed like a good
    idea to me.

--- wake.py
+++ wake.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env pythonw2.6
 from time import sleep
-from datetime import datetime, time
+from datetime import datetime, time, timedelta
 from sys import argv
 
 def wake():
@@ -14,15 +14,19 @@ def format(delt):
   sec = sec % 3600
   min = int(sec / 60)
   sec = sec % 60
-  return "%d:%d:%d" % (hours, min, sec)
+  return "%02d:%02d:%02d" % (hours, min, sec)
 
 assert len(argv) > 1, "Please supply a time of the format hh:mm"
 target = [ int(x) for x in argv[1].split(":") ]
-assert len(target) == 2, "Please supply a valid time in the form hh:mm"
+assert len(target) == 2 or len(target) == 3, "Please supply a valid time in the form hh:mm"
 targetDate = datetime.combine(datetime.today(), time(*target))
-assert targetDate > datetime.now(), "Time must be in the future."
+
+if targetDate < datetime.now(): targetDate += timedelta(days=1)
+print "Waking at %s" % targetDate
+assert targetDate >= datetime.now(), "Time must be in the future."
+
 delt = targetDate - datetime.now()
-assert delt.days == 0, "Time is not until tomorrow.  That's not going to work."
+assert delt.days == 0, "Time is more than a day into the future.  That's not going to work."
 print "sleeping for %s" % format(delt)
 sleep(delt.seconds)
 wake()
